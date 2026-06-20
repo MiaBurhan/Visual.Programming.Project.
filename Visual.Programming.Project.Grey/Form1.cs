157caf4ca39538a8d756d8128e936fe261a0c025
tusadiq haider <tusadiqhaider@gmail.com>
2026-06-20 18:24:24 +0500

    hallo burhan ma na kar dia ha
    Redesign Form1 as HomeForm with e-commerce UI
    
    Renamed Form1 to HomeForm and overhauled the UI to mimic a modern e-commerce homepage. Added banner, perks, best sellers, and product cards with images, ratings, and "Add to Cart" buttons. Updated InitializeComponent, event handlers, and set HomeForm as the new startup form. Adjusted layout, scaling, and form properties for the new design.

--- Visual.Programming.Project.Grey/Form1.cs
+++ Visual.Programming.Project.Grey/Form1.cs
@@ -1,10 +1,22 @@
 namespace Visual.Programming.Project.Grey
 {
-    public partial class Form1 : Form
+    public partial class HomeForm : Form
     {
-        public Form1()
+        public HomeForm() => InitializeComponent();
+
+        private void label1_Click(object sender, EventArgs e)
         {
-            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
