cc3ee42753d854881b8b4412bf190c43a17eff82
BurhanPC <bmia2684@gmail.com>
2026-06-20 13:36:54 +0500

    wesa

--- Visual.Programming.Project.Grey/Form1.Designer.cs
+++ Visual.Programming.Project.Grey/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(228, 73);
+            label1.Location = new Point(209, 192);
             label1.Name = "label1";
             label1.Size = new Size(259, 30);
             label1.TabIndex = 0;
